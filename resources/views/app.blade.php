d6a85f153c64a417e64bb619f38643060e28d970
David Janeček <davidjanecek123@seznam.cz>
2025-07-14 13:16:48 +0200

    feat: Inertia, Vue & TailwindCSS integration

---
+++ resources/views/app.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="cs">
+<head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <title inertia>{{ config('app.name', 'Autoauto.cz') }}</title>
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+    @inertiaHead
+</head>
+<body class="font-sans antialiased">
+    @inertia
+</body>
+</html>
