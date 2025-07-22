00cb1e8a4d4da1a53db351bf3dce3cc1f32c66f6
ludwigtomas <ludwigtomas@seznam.cz>
2025-07-22 17:00:14 +0200

    Fix: update footer text

--- resources/js/layouts/partials/Footer.vue
+++ resources/js/layouts/partials/Footer.vue
@@ -2,8 +2,8 @@
 
 <template>
     <footer class="fixed bottom-0 left-0 z-50 w-full translate-y-0 opacity-100">
-        <div class="bg-gray-50 py-3 shadow-lg shadow-gray-100 text-center">
-            Open source project by
+        <div class="bg-gray-50 py-3 text-center shadow-lg shadow-gray-100">
+            Starter Kit by
             <a
                 class="text-blue-500 hover:underline"
                 href="https://devuni.cz/"
